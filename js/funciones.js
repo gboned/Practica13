71450ba7145fb12d3a52f5cb2a02dea442237fe2
gboned <gboned@cifpfbmoll.eu>
2020-06-08 19:38:01 +0200

    Añadir función que obtiene el número del día de la semana

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// Añadir función para obtener el día de la semana actual.
+function obtenerDiaActual() {
+    var diaSemanaActual = new Date();
+    return diaSemanaActual.getDay();  
+}
