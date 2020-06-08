c7a941c6fcc492e862f4f8e2d5cf1932318444a1
gboned <gboned@cifpfbmoll.eu>
2020-06-08 20:16:05 +0200

    Añadir función para seleccionar día actual

--- js/funciones.js
+++ js/funciones.js
@@ -5,3 +5,11 @@ function obtenerDiaActual() {
     var diaSemanaActual = new Date();
     return diaSemanaActual.getDay();  
 }
+
+// Añadir función para que según el día que sea, se seleccione este por defecto.
+function seleccionarDia() {
+    // Creo una variable que seleccione la opción que coincide con el día de la semana actual.
+    var elementoDia = document.querySelector(`input[value="${obtenerDiaActual()}"]`);
+    // Añado al elemento seleccionado un atributo checked a true, para que salga seleccionado por defecto el día actual.
+    elementoDia.setAttribute("checked", true);
+}
